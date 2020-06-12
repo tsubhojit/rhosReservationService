2467dbc1f58494ffbc45342593622dc28e5cfa02
root <root@devopsclient.tcs.io>
2020-06-12 11:36:08 -0500

    minor change for Tekton POC

--- src/main/java/com/rhos/rhosReservationService/controllers/ManageBookingController.java
+++ src/main/java/com/rhos/rhosReservationService/controllers/ManageBookingController.java
@@ -2,7 +2,9 @@ package com.rhos.rhosReservationService.controllers;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
